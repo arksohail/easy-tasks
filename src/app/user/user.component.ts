3f36e8a5c1fff94ac96c9257b4ed85cbe8bbe6a0
arksohail <arksohail@gmail.com>
2024-09-15 01:02:09 +0530

    Using @Input and @Output to Manage and emit User Data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,26 +11,26 @@ import { Component, computed, Input, signal, input, Output, EventEmitter, output
 })
 export class UserComponent {
 
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
   @Input({required: true}) id!: string;
-  // @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter();
+  
 
-  select = output<string>();
+  
+  // select = output<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // imagePath = computed(() => 'assets/users/' + this.avatar());
 
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  imagePath = computed(() => 'assets/users/' + this.avatar());
 
   // selectedUsers = signal(DUMMY_USERS[randomIndex]);
   // imagePath = computed(() => 'assets/users/' + this.selectedUsers().avatar); // Using Signals to read computed values and listen to change detection
 
   // Old Way of using angular's Change Detection Mechanism "zone"
-  // get imagePath(): string {
-    // return 'assets/users/' + this.avatar();
-  // }
+  get imagePath(): string {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser(): void {
     this.select.emit(this.id);
