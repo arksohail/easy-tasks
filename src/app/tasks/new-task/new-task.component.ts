94d9d293127af56d86f83587696ae788a23c83c4
arksohail <arksohail@gmail.com>
2024-09-15 20:50:31 +0530

    Using Directives & Two Way Data Binding using NgModel by importing FormsModule

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,20 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>(); 
+  @Output() cancel = new EventEmitter<void>();
+  @Output() createTask = new EventEmitter()
+
+  eneteredTitle = '';
+  eneteredSummary = '';
+  eneteredDate = '';
 
   onCancelAddTask() {
     this.cancel.emit();
